42ef0d1eac26b6a6ac209fc3486dfa323fc4f957
Matt Megarry <mattmegarry@gmail.com>
2018-02-28 14:02:57 +0100

    routes, guards and auth service - partially done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '',  component: HomePageComponent, canActivate: [ InitAuthGuardService ] },
+  { path: 'login',  component: AuthLoginPageComponent, canActivate: [ RequireAnonGuardService ] },
+  { path: 'signup',  component: AuthSignupPageComponent, canActivate: [ RequireAnonGuardService ] },
+  /* { path: 'page',  component: ... , canActivate: [ RequireUserGuardService ] }, */
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
