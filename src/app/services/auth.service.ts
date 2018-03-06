b808e67aa84db26ce5a174673985fcc2760cae20
Matt Megarry <mattmegarry@gmail.com>
2018-03-06 13:12:19 +0100

    FE deployment preparation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/toPromise';
 
+import { environment } from '../../environments/environment';
+
 // import { environment } from '../../environments/environment';
 
 @Injectable()
@@ -12,7 +14,7 @@ export class AuthService {
   private user: any;
   private userChange: Subject<any> = new Subject();
 
-  private API_URL = 'http://localhost:3000/auth';
+  private apiUrl = environment.apiUrl + '/auth';
 
   userChange$: Observable<any> = this.userChange.asObservable();
 
@@ -28,7 +30,7 @@ export class AuthService {
     const options = {
       withCredentials: true
     };
-    return this.httpClient.get(`${this.API_URL}/me`, options)
+    return this.httpClient.get(`${this.apiUrl}/me`, options)
       .toPromise()
       .then((user: any) =>{ 
         if (user.error) {
@@ -49,7 +51,7 @@ export class AuthService {
     const options = {
       withCredentials: true
     };
-    return this.httpClient.post(`${this.API_URL}/login`, user, options)
+    return this.httpClient.post(`${this.apiUrl}/login`, user, options)
       .toPromise()
       .then((data) => this.setUser(data));
   }
@@ -58,7 +60,7 @@ export class AuthService {
     const options = {
       withCredentials: true
     };
-    return this.httpClient.post(`${this.API_URL}/signup`, user, options)
+    return this.httpClient.post(`${this.apiUrl}/signup`, user, options)
       .toPromise()
       .then((data) => this.setUser(data));
   }
@@ -67,7 +69,7 @@ export class AuthService {
     const options = {
       withCredentials: true
     };
-    return this.httpClient.post(`${this.API_URL}/logout`, {}, options)
+    return this.httpClient.post(`${this.apiUrl}/logout`, {}, options)
       .toPromise()
       .then(() => this.setUser());
   }
